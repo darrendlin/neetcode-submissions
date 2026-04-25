c3fa953b49322b0c47c8f92c32bb050735e33195
Darren Lin <lin.darren95@gmail.com>
2026-04-25 11:26:55 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l_offset = 0
+        r_offset = 0
+
+        while l_offset < len(s) - 1 - r_offset:
+
+            left = s[l_offset].lower()
+            while not left.isalnum():
+                l_offset += 1
+                if l_offset >= len(s):
+                    return True
+                left = s[l_offset].lower()
+            right = s[len(s) - 1 - r_offset].lower()
+            while not right.isalnum():
+                r_offset += 1
+                if len(s) - 1 - r_offset < 0:
+                    return True
+                right = s[len(s) - 1 - r_offset].lower()
+            if l_offset >= len(s) - 1 - r_offset:
+                break
+            if left != right:
+                return False
+            l_offset += 1
+            r_offset += 1
+        return True
