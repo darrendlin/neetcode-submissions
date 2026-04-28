147f1d4e80a5cd8828a5a9e300afa4a18affc80d
Darren Lin <lin.darren95@gmail.com>
2026-04-27 21:04:55 -0400

    Add: maximal-square - submission-1

---
+++ Data Structures & Algorithms/maximal-square/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        h = len(matrix)
+        w = len(matrix[0])
+        max_sq = 0
+        
+        res = [[0] * w for i in range(h)]
+
+        for i in range(h-1, -1, -1):
+            for j in range(w-1, -1, -1):
+                #print(i, j, res)
+                if matrix[i][j] == "0":
+                    res[i][j] = 0
+                    continue
+                right, down, diag = 0, 0, 0
+                if j < w-1:
+                    right = res[i][j+1]
+                if i < h-1:
+                    down = res[i+1][j]
+                if right > 0 and down > 0:
+                    diag = res[i+1][j+1]
+
+                sq = 1 + min([right, down, diag])
+                res[i][j] = sq
+                max_sq = max(max_sq, sq)
+                
+        return max_sq**2
+                
