3d03a7eb363ba637093db7b331a58fa9b96af19a
Darren Lin <lin.darren95@gmail.com>
2026-04-29 22:44:18 -0400

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        record = [(temperatures[0], 0)]  # pairs of (temp, index)
+        out = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            while len(record) > 0 and record[-1][0] < temperatures[i]:
+                popped = record.pop()
+                out[popped[1]] = i - popped[1]
+
+            record.append((temperatures[i], i))
+
+        return out
